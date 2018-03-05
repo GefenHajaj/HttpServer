551e93e37be921a8dd49470ba98f4661418cfe7a
GefenHajaj <gefenhajaj@gmail.com>
2018-03-05 22:23:29 +0200

    Port change

--- Constants.py
+++ Constants.py
@@ -5,7 +5,7 @@ Part of http_server.py
 import os.path
 
 IP = '0.0.0.0'
-PORT = 55556
+PORT = 55555
 ROOT_DIR = 'D:' + os.path.sep + 'wwwroot'
 REQUEST_LENGTH = 1024  # bytes
 SOCKET_TIMEOUT = 1  # seconds to wait for sending/receiving
